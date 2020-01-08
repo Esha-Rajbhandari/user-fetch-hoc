08cdd996f34ca649fbbedcd407f890f8fb59004a
Esha-Rajbhandari <esha9277@gmail.com>
2020-01-08 12:33:41 +0545

    Updates

---
+++ precache-manifest.9a8bedb562bd251009bf7bdb7c486d68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ddbbbaf8885c8baa443d38f416395e1",
+    "url": "/user-fetch-hoc/index.html"
+  },
+  {
+    "revision": "43054154142def083256",
+    "url": "/user-fetch-hoc/static/css/main.bf69244d.chunk.css"
+  },
+  {
+    "revision": "fba7ff9c516b29e98642",
+    "url": "/user-fetch-hoc/static/js/2.3b0e13b5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/user-fetch-hoc/static/js/2.3b0e13b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43054154142def083256",
+    "url": "/user-fetch-hoc/static/js/main.b9e8997d.chunk.js"
+  },
+  {
+    "revision": "1dc492374795814aa868",
+    "url": "/user-fetch-hoc/static/js/runtime-main.2904b511.js"
+  }
+]);
